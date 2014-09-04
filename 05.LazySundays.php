14b077e3160e8723a5a0efd2d3f4b183119e0a5a
hanbozdugan <lz2jvc@abv.bg>
2014-09-04 19:35:33 +0300

    Delete 05.LazySundays.php

--- 05.LazySundays.php
+++
@@ -1,8 +0,0 @@
-<?php
-$currentMonth = date('m');
-$timestampSunday = strtotime("first Sunday of this month");
-do {
-$sundayDate = date('jS F, Y', $timestampSunday);
-echo $sundayDate . "\n";
-$timestampSunday += 7 * 24 * 60 * 60;
-} while ($currentMonth == date('m', $timestampSunday));
